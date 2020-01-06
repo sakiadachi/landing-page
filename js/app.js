3650e706f4bcf424588cc21d54dce5e47e717c00
sakiadachi <58318131+sakiadachi@users.noreply.github.com>
2020-01-06 17:17:22 +0100

    2nd
    modified code

--- js/app.js
+++ js/app.js
@@ -42,48 +42,44 @@ function addElement (){
     const sections = document.getElementsByClassName("section")
     for (let section of sections) {
         const navli = document.createElement("li");
+        navli.className = "menu__link";
         const anchor = document.createElement("a");
         anchor.href = "#" + section.id;
         anchor.textContent = section.dataset.nav;
+        navli.dataset.sectionId = section.id;
         navli.appendChild(anchor);
         navbar.appendChild(navli);
+
+
     }
 }
 
-
-window.onscroll = function() {myFunction()};
-
-
-var sticky = navbar.offsetTop;
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-    }
+window.onscroll = function() {
+  
+  const sections = document.getElementsByClassName("section");
+  const distances = [];
+  for (let section of sections) {
+    const distance = Math.abs(section.getBoundingClientRect().top);
+    distances.push([section, distance]);
   }
+  distances.sort(function(a, b) {return a[1] - b[1]})
+  const closestSection = distances[0][0];
+  const navlis = document.getElementsByClassName("menu__link");
+  for (let navli of navlis) {
+     if (navli.dataset.sectionId === closestSection.id) {
+       navli.classList.add("active");
+     } else {
+      navli.classList.remove("active");
+     }
+  }
+};
+
+
+
 
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-(function (){
-    "use strict";
-    let target = document.querySelectorAll('a[href^="#"]');
-    let i = 0;
-    for (i = 0; i < target.length; i++){
-        e.preventDefault();
-        target[i].onclick = function (e){
-        let hash = this.getAttribute("href");
-        let elem = document.getElementById(hash.replace ("#",""));
-        //history.pushState (null, null, hush);
-        elem.scrollIntoView({ behavior: 'smooth'});
-      }
-}
-}) ();
-
-
-
 
 /**
  * End Main Functions
@@ -91,13 +87,24 @@ function myFunction() {
  * 
 */
 window.onload = function() {
-    this.addElement()
-}
+    this.addElement();
+};
 
 // Build menu 
+var sticky = navbar.offsetTop;
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
 
-// Scroll to section on link click
+// add CSS style for menu
+var LiStyle = document.querySelector(".navbar__menu ul");
+LiStyle.style.cssText = "padding-left: 10px";
+
+var navStyle = document.querySelector(".navbar__menu");
+navStyle.style.cssText = "background-color: grey;"
 
 // Set sections as active
-
-
