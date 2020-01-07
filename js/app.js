83d9a2178c999e191c9dff18c43d1a59644535ba
sakiadachi <58318131+sakiadachi@users.noreply.github.com>
2020-01-07 16:52:26 +0100

    third commit
    Edited the nav var style and added the active class.
    Created the fourth section on the page.

--- js/app.js
+++ js/app.js
@@ -33,11 +33,10 @@
  * 
 */
 
-// build the nav"
 
 //Get the nav bar
-var navbar = document.getElementById("navbar__list")
-
+const navbar = document.getElementById("navbar__list")
+//Build the nav list
 function addElement (){
     const sections = document.getElementsByClassName("section")
     for (let section of sections) {
@@ -49,37 +48,30 @@ function addElement (){
         navli.dataset.sectionId = section.id;
         navli.appendChild(anchor);
         navbar.appendChild(navli);
-
-
     }
 }
 
+//add the scroll function
 window.onscroll = function() {
-  
-  const sections = document.getElementsByClassName("section");
-  const distances = [];
-  for (let section of sections) {
-    const distance = Math.abs(section.getBoundingClientRect().top);
-    distances.push([section, distance]);
+    // Add class 'active' to section when near top of viewport
+    const sections = document.getElementsByClassName("section");
+    const distances = [];
+    for (let section of sections) {
+        const distance = Math.abs(section.getBoundingClientRect().top);
+       distances.push([section, distance]);
   }
-  distances.sort(function(a, b) {return a[1] - b[1]})
-  const closestSection = distances[0][0];
-  const navlis = document.getElementsByClassName("menu__link");
-  for (let navli of navlis) {
-     if (navli.dataset.sectionId === closestSection.id) {
-       navli.classList.add("active");
-     } else {
-      navli.classList.remove("active");
-     }
+// Set sections as active
+    distances.sort(function(a, b) {return a[1] - b[1]})
+    const closestSection = distances[0][0];
+    const navlis = document.getElementsByClassName("menu__link");
+    for (let navli of navlis) {
+        if (navli.dataset.sectionId === closestSection.id) {
+        navli.classList.add("active");
+        } else {
+        navli.classList.remove("active");
+        }
   }
 };
-
-
-
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
 
 /**
  * End Main Functions
@@ -90,21 +82,37 @@ window.onload = function() {
     this.addElement();
 };
 
-// Build menu 
-var sticky = navbar.offsetTop;
+// Build menu bar
+const sticky = navbar.offsetTop;
 function myFunction() {
     if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
+        navbar.classList.add("sticky");
     } else {
-      navbar.classList.remove("sticky");
+        navbar.classList.remove("sticky");
     }
-  }
+}
 
-// add CSS style for menu
-var LiStyle = document.querySelector(".navbar__menu ul");
+// Add CSS style for menu
+const LiStyle = document.querySelector(".navbar__menu ul");
 LiStyle.style.cssText = "padding-left: 10px";
 
-var navStyle = document.querySelector(".navbar__menu");
-navStyle.style.cssText = "background-color: grey;"
+const navStyle = document.querySelector(".navbar__menu");
+navStyle.style.cssText = "background-color: white"
 
-// Set sections as active
+// Create section4
+const container = document.querySelector("main");
+
+// Clone the existed contents
+const contents = document.querySelector(".section");
+const newSection = contents.cloneNode(true);
+
+container.appendChild(newSection);
+
+// Change the attribute's names
+newSection.className = "section";
+newSection.setAttribute("data-nav", "section 4"); 
+newSection.setAttribute("id", "section4"); 
+
+// Change the heading in section4
+var newHeading = newSection.querySelector("h2");
+newHeading.textContent = "Section 4"
